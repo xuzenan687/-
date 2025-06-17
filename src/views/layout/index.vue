5aef08c3c3fd29719641d883fbd5dea6e36bea41
xuziran <xuziran687@gmail.com>
2025-06-17 13:20:45 +0800

    feat: initialize Vue project with Vite, Element Plus, and basic structure
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Included favicon.ico for the application.
    - Created main application component (App.vue) with router view.
    - Added assets including background image and logo.
    - Implemented main.js to set up Vue app with router and Element Plus.
    - Configured router with multiple views including index, class, department, employee, log, student, and reports.
    - Developed layout component (LayoutView.vue) with a sidebar and header.
    - Created placeholder views for class management, department management, employee management, log management, student management, and reports.
    - Designed login page with form validation and styling.
    - Added Vite configuration for resolving paths.

---
+++ src/views/layout/index.vue
@@ -0,0 +1,113 @@
+<script setup></script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <!-- Header 区域 -->
+      <el-header class="header">
+        <span class="title">Tlias智能学习辅助系统</span>
+        <span class="right_tool">
+          <a href="">
+            <el-icon><EditPen /></el-icon> 修改密码 &nbsp;&nbsp;&nbsp; |
+            &nbsp;&nbsp;&nbsp;
+          </a>
+          <a href="">
+            <el-icon><SwitchButton /></el-icon> 退出登录
+          </a>
+        </span>
+      </el-header>
+
+      <el-container>
+        <!-- 左侧菜单栏 -->
+        <el-aside width="200px" class="aside">
+          <el-menu router>
+            <!-- 首页菜单 -->
+            <el-menu-item index="/index">
+              <el-icon><Promotion /></el-icon> 首页
+            </el-menu-item>
+            <!-- 班级管理菜单 -->
+            <el-sub-menu index="/manage">
+              <template #title>
+                <el-icon><Menu /></el-icon> 班级学员管理
+              </template>
+              <el-menu-item index="/clazz">
+                <el-icon><HomeFilled /></el-icon>班级管理
+              </el-menu-item>
+              <el-menu-item index="/stu">
+                <el-icon><UserFilled /></el-icon>学员管理
+              </el-menu-item>
+            </el-sub-menu>
+            <!-- 系统信息管理 -->
+            <el-sub-menu index="/system">
+              <template #title>
+                <el-icon><Tools /></el-icon>系统信息管理
+              </template>
+              <el-menu-item index="/dept">
+                <el-icon><HelpFilled /></el-icon>部门管理
+              </el-menu-item>
+              <el-menu-item index="/emp">
+                <el-icon><Avatar /></el-icon>员工管理
+              </el-menu-item>
+            </el-sub-menu>
+            <!-- 数据统计管理 -->
+            <el-sub-menu index="/report">
+              <template #title>
+                <el-icon><Histogram /></el-icon>数据统计管理
+              </template>
+              <el-menu-item index="/empReport">
+                <el-icon><InfoFilled /></el-icon>员工信息统计
+              </el-menu-item>
+              <el-menu-item index="/stuReport">
+                <el-icon><Share /></el-icon>学员信息统计
+              </el-menu-item>
+              <el-menu-item index="/log">
+                <el-icon><Document /></el-icon>日志信息统计
+              </el-menu-item>
+            </el-sub-menu>
+          </el-menu>
+        </el-aside>
+
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background-image: linear-gradient(
+    to right,
+    #00547d,
+    #007fa4,
+    #00aaa0,
+    #00d072,
+    #a8eb12
+  );
+}
+
+.title {
+  color: white;
+  font-size: 40px;
+  font-family: 楷体;
+  line-height: 60px;
+  font-weight: bolder;
+}
+
+.right_tool {
+  float: right;
+  line-height: 60px;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+}
+
+.aside {
+  width: 220px;
+  border-right: 1px solid #ccc;
+  height: 730px;
+}
+</style>
